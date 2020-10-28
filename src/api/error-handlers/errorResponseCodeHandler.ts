31db0a4dc2cbc4db615bcca026561941da8c5c50
Aaron Yarborough <me@aaronjy.me>
2020-10-28 19:51:22 +0100

    Add ConflictError

--- src/api/error-handlers/errorResponseCodeHandler.ts
+++ src/api/error-handlers/errorResponseCodeHandler.ts
@@ -1,15 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { NotFoundError } from '../../errors/notFoundError';
 import { JsonPatchError } from 'fast-json-patch';
+import { ConflictError } from '../../errors/conflictError';
 
 export function errorResponseCodeHandler(err: Error, req: Request, resp: Response, next: NextFunction): void {
     if (err instanceof NotFoundError) {
         resp.status(404);
+    } else if (err instanceof ConflictError) {
+        resp.status(409);
     } else if (err instanceof JsonPatchError) {
         resp.status(400);
     } else {
         resp.status(500);
     }
+
 
     next(err);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { errorResponseCodeHandler } from './api/error-handlers/errorResponseCode
 export { errorResponseHandler } from './api/error-handlers/errorResponseHandler';
 export { GalleryVisibility } from './enums/galleryVisibility';
 export { NotFoundError } from './errors/notFoundError';
+export { ConflictError } from './errors/conflictError';
